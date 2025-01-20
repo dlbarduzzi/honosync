aa37ad1a2ec9a2b0cbfe77c3aa06aebf8d3f1420
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-01-20 10:22:38 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      semi: false,
+      indent: 2,
+      quotes: "double",
+    },
+  },
+  {
+    rules: {
+      "style/arrow-parens": "off",
+      "ts/no-unused-vars": "error",
+      "ts/no-explicit-any": "error",
+      "max-len": ["error", { code: 88 }],
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+    },
+  },
+)
